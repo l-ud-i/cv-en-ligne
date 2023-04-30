50b3b3cb5de6539734f09d5ee5af01149beff987
Ludi <ludi.adressemail@gmail.com>
2023-04-30 23:07:08 +0200

    add new pages

---
+++ counter.js
@@ -0,0 +1,41 @@
+
+// let count = 5;
+
+// count = count + 5;
+
+// console.log(count);
+
+
+// initialize the count to 0
+let count = 0;
+
+// listen for clicks on the increment button
+
+const button = document.getElementById('js-increment-btn');
+button.addEventListener('click', increment);
+
+// increment the count variable when the button is clicked (log it out)
+// change the count in the HTML to reflect the new count
+function increment(event) {
+    count++;
+    document.querySelector("#counter").textContent = count;
+}
+
+// save the count
+const saveBtn = document.getElementById('js-save-btn');
+saveBtn.addEventListener('click', save);
+
+function save(event) {
+    document.getElementById('js-text-save').textContent = count;
+    count = 0;
+}
+
+
+// log out the count
+const resetBtn = document.getElementById('js-reset-btn');
+resetBtn.addEventListener('click', reset);
+
+function reset(event) {
+    document.querySelector("#counter").textContent = 0;
+    count = 0;
+}
